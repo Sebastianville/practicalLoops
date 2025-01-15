bde2d5660c001dfb4f0abd777a0dd3722c7a5679
Sebastianville <sebastian.villegas2623@gmail.com>
2025-01-15 14:27:31 -0500

    part 2 complete

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ for (let i =1; i < 101; i++){
         console.log(i);
     }
 }
+
+
+//part 2
+let n = parseInt(prompt("Enter a number of your choice"))
+let plusONe = n + 1
+
+while(true){
+    let isPrime = true; 
+    for (let i =2; i < plusONe; i++){
+        if (plusONe % i === 0) {
+            isPrime = false; 
+            break
+        }
+    }
+    if (isPrime) {
+        console.log(`The next prime number after ${n} is ${plusONe}`);
+        break
+    }
+    plusONe++
+}
+
