2ad2113a68e1d968f1715e4ceafe57ee9a5c36ad
Sebastianville <sebastian.villegas2623@gmail.com>
2025-01-16 16:03:24 -0500

    part 3 completed

--- index.js
+++ index.js
@@ -30,3 +30,44 @@ while(true){
     plusONe++
 }
 
+// part 3
+
+
+let csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+//each of our cells 
+let cell1 = ''
+let cell2 = ''
+let cell3 = ''
+let cell4 = ''
+
+//where we are in the cell
+let curCell = 1;
+
+//Looping through csv
+for (let i = 0; i < csv.length; i++){
+    let curCharacter = csv[i]
+    // console.log(curCharacter)
+    if(curCharacter === ","){
+        curCell++
+        // console.log(curCharacter){
+    } else if (curCharacter === '\n') {
+        console.log(cell1, cell2, cell3, cell4)
+        cell1 = ""
+        cell2 = ""
+        cell3 = ""
+        cell4 = ""
+        curCell = 1
+    } else {
+        if (curCell === 1){
+            cell1 += curCharacter
+        } else if (curCell === 2){
+            cell2 +=  curCharacter
+        } else if (curCell === 3) {
+            cell3 += curCharacter
+        } else if (curCell === 4) {
+            cell4 += curCharacter
+        }
+    } 
+
+} //console.log(cell1)
